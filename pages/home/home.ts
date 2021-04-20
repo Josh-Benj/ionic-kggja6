92f6c6054c177a744c7742f8c525cefccce1bc64
Josh-Benj <joshua.miguel101195@my.lsgh.edu.ph>
2021-04-20 23:51:58 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -16,30 +16,30 @@ export class HomePage {
 InterestRate: any;
 PaymentTerms: number;
   constructor(public navCtrl: NavController) {}
+    calculateOldPhoneAppraised(){
+    this.OldPhoneAppraised = (this.FinancialAmount*this.InterestRate)/this.PaymentTerms;
+    this.OldPhoneAppraised = parseFloat(this.OldPhoneAppraised.toFixed(1));
 
+    if (this.OldPhoneAppraised <18.5) {
+      this.OldPhoneAppraisedMessage = "Old Phone Appraised Value";
+    } else if(this.OldPhoneAppraised > 18.5 && this.OldPhoneAppraised < 25) {
+      this.OldPhoneAppraisedMessage = "Old Phone Appraised Value";
+    }else if (this.OldPhoneAppraised >= 25 && this.OldPhoneAppraised < 30) {
+    }
+    }    
   calculateFinancialAmount(){
     this.FinancialAmount = this.Gadget - this.OldPhone;
     this.FinancialAmount = parseFloat(this.FinancialAmount.toFixed(2));
 
     if (this.FinancialAmount <18.5) {
-      this.FinancialAmountMessage = "Old Phone Appraised Value";
+      this.FinancialAmountMessage = "Financial Amount";
     } else if (this.FinancialAmount > 18.5 && this.FinancialAmount < 25) {
       this.FinancialAmountMessage = "Financed Amount";
     }else if (this.FinancialAmount >= 25 && this.FinancialAmount < 30) {
     }
   }
   
-    calculateOldPhoneAppraised(){
-    this.OldPhoneAppraised = (this.FinancialAmount*this.InterestRate)/this.PaymentTerms;
-    this.OldPhoneAppraised = parseFloat(this.FinancialAmount.toFixed(2));
 
-    if (this.OldPhoneAppraised <18.5) {
-      this.OldPhoneAppraisedMessage = "Old Phone Appraised Value";
-    } else if (this.OldPhoneAppraised > 18.5 && this.OldPhoneAppraised < 25) {
-      this.OldPhoneAppraisedMessage = "Financed Amount";
-    }else if (this.OldPhoneAppraised >= 25 && this.OldPhoneAppraised < 30) {
-    }
-    }    
       
     
 }
