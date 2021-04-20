88c7b4dd4ad43d658bc0c38c58aa2eeb528dfc53
Josh-Benj <joshua.miguel101195@my.lsgh.edu.ph>
2021-04-21 00:03:32 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -9,33 +9,29 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   Gadget: number;
   OldPhone: number;
-  FinancialAmount: number;
-  FinancialAmountMessage: string;
+  FinancedAmount: number;
+  FinancedAmountMessage: string;
   OldPhoneAppraised: number;
   OldPhoneAppraisedMessage: string;
 InterestRate: any;
 PaymentTerms: number;
   constructor(public navCtrl: NavController) {}
     calculateOldPhoneAppraised(){
-    this.OldPhoneAppraised = (this.FinancialAmount*this.InterestRate)/this.PaymentTerms;
+    this.OldPhoneAppraised = (this.FinancedAmount*this.InterestRate)/this.PaymentTerms;
     this.OldPhoneAppraised = parseFloat(this.OldPhoneAppraised.toFixed(1));
 
     if (this.OldPhoneAppraised <18.5) {
       this.OldPhoneAppraisedMessage = "Old Phone Appraised Value";
-    } else if(this.OldPhoneAppraised > 18.5 && this.OldPhoneAppraised < 25) {
-      this.OldPhoneAppraisedMessage = "Old Phone Appraised Value";
-    }else if (this.OldPhoneAppraised >= 25 && this.OldPhoneAppraised < 30) {
+    
     }
     }    
   calculateFinancialAmount(){
-    this.FinancialAmount = this.Gadget - this.OldPhone;
-    this.FinancialAmount = parseFloat(this.FinancialAmount.toFixed(2));
+    this.FinancedAmount = this.Gadget - this.OldPhone;
+    this.FinancedAmount = parseFloat(this.FinancedAmount.toFixed(2));
 
-    if (this.FinancialAmount <18.5) {
-      this.FinancialAmountMessage = "Financial Amount";
-    } else if (this.FinancialAmount > 18.5 && this.FinancialAmount < 25) {
-      this.FinancialAmountMessage = "Financed Amount";
-    }else if (this.FinancialAmount >= 25 && this.FinancialAmount < 30) {
+    if (this.FinancedAmount <18.5) {
+      this.FinancedAmountMessage = "Financed Amount";
+    
     }
   }
   
