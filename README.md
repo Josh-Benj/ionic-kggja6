124f96cb0960d295123f2afa8962b59a2f78e7ec
Josh-Benj <joshua.miguel101195@my.lsgh.edu.ph>
2021-04-20 11:21:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-kggja6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-kggja6)
---
+++ pages/home/home.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
+})
+export class HomePage {
+  height: number;
+  weight: number;
+  bmi: number;
+  bmiMessage: string;
+  
+  constructor(public navCtrl: NavController) {}
+
+  calculateBMI(){
+    this.bmi = this.weight / (this.height * this.height);
+    this.bmi = parseFloat(this.bmi.toFixed(2));
+
+    if (this.bmi <18.5) {
+      this.bmiMessage = "Financed Amount";
+    } else if (this.bmi > 26) {
+      this.bmiMessage = "Total Interest Payable";
+    }else if (this.bmi >= 25 && this.bmi < 30) {
+      this.bmiMessage = "Overweight";
+    } else {
+      this.bmiMessage = "Obese";
+    }
+      
+      
+    
+}
+}
