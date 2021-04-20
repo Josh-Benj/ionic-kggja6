c6d2afa66d2d193a2239e16d9a8f6e6c753d7084
Josh-Benj <joshua.miguel101195@my.lsgh.edu.ph>
2021-04-21 00:28:22 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -11,19 +11,20 @@ export class HomePage {
   OldPhone: number;
   FinancedAmount: number;
   FinancedAmountMessage: string;
-  OldPhoneAppraised: number;
-  OldPhoneAppraisedMessage: string;
+  OldPhoneAppraisedValue: number;
+  OldPhoneAppraisedValueMessage: string;
   MonthlyPayment: number;
   MonthlyPaymentMessage: string;
-InterestRate: 1.30;
+InterestRate: any;
 PaymentTerms: number;
+OldPhoneAppraised: number;
   constructor(public navCtrl: NavController) {}
-    calculateOldPhoneAppraised(){
-    this.OldPhoneAppraised = (this.FinancedAmount*this.InterestRate)*this.PaymentTerms;
-    this.OldPhoneAppraised = parseFloat(this.OldPhoneAppraised.toFixed(1));
+    calculateOldPhoneAppraisedValue(){
+    this.OldPhoneAppraisedValue = (this.FinancedAmount*this.InterestRate)*this.PaymentTerms;
+    this.OldPhoneAppraisedValue = parseFloat(this.OldPhoneAppraised.toFixed(1));
 
     if (this.OldPhoneAppraised <4000) {
-      this.OldPhoneAppraisedMessage = "Old Phone Appraised Value";
+      this.OldPhoneAppraisedValueMessage = "Old Phone Appraised Value";
     
     }
     }    
