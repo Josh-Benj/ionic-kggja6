79804facd67693ec118fc6c70c12add0977d239c
Josh-Benj <joshua.miguel101195@my.lsgh.edu.ph>
2021-04-21 12:19:55 +0800

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -17,11 +17,13 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {}
     calculateOldPhoneAppraisedValue(){
-    this.opav = (this.fa*this.ir)*this.pt;
+    this.opav = (this.fa*this.ir/100)*this.pt;
     this.opav = parseFloat(this.opav.toFixed(2));
 
     this.fa= this.gp-this.opav;
     this.fa = parseFloat(this.fa.toFixed(2));
     
+    this.mp= (this.fa+this.ir)/this.pt;
+    this.mp = parseFloat(this.mp.toFixed(2));
     }
     }    
