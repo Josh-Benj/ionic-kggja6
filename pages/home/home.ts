fcc38e6b7dee943fd4f20b555f535b782d41c84c
Josh-Benj <joshua.miguel101195@my.lsgh.edu.ph>
2021-04-21 12:50:33 +0800

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -17,16 +17,20 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {}
     calculateTTULC(){
+    this.fa = this.gp-this.opav;
+    this.fa = parseFloat(this.fa.toFixed(2));
+
+    this.ir = 1.30;
+    this.ir = parseFloat(this.ir.toFixed(2));
+
     this.tip = (this.fa*this.ir/100)*this.pt;
     this.tip = parseFloat(this.tip.toFixed(2));
 
-    this.fa = this.gp-this.opav;
-    this.fa = parseFloat(this.fa.toFixed(2));
+    
     
     this.mp = (this.fa+this.ir)/this.pt;
     this.mp = parseFloat(this.mp.toFixed(2));
 
-    this.ir = 1.30
-    this.ir = parseFloat(this.ir.toFixed(2));
+    
     }
     }    
