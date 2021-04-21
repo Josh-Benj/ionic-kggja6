f4e017fef86d78a61d0fc407157ddb45c397d26f
Josh-Benj <joshua.miguel101195@my.lsgh.edu.ph>
2021-04-21 13:47:55 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -9,7 +9,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   gp: number;
   opav: number;
+  opav2: number;
   pt: number;
+
   fa: number;
   ir: number;
   tip: number;
@@ -17,20 +19,21 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {}
     calculateTTULC(){
+
     this.fa = this.gp-this.opav;
     this.fa = parseFloat(this.fa.toFixed(2));
 
     this.ir = 1.30;
     this.ir = parseFloat(this.ir.toFixed(2));
 
-    this.tip = (this.fa*this.ir/100)*this.pt;
+    this.tip = ((this.fa*this.ir)/100)*this.pt;
     this.tip = parseFloat(this.tip.toFixed(2));
-
     
-    
-    this.mp = (this.fa+this.ir)/this.pt;
+    this.mp = (((this.fa*100)/100)+((this.tip*100)/100))/this.pt;
     this.mp = parseFloat(this.mp.toFixed(2));
 
-    
+    this.opav2 = this.opav;
+    this.opav2 = parseFloat(this.opav2.toFixed(2));
+
     }
-    }    
+    }
